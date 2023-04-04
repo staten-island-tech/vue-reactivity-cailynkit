1cf9d7ec34c3b86e339cabf1fd87840ee089f32f
cailynkit <cailynkitchens8@gmail.com>
2023-04-04 13:49:00 -0400

    css?? imgs??

--- vue-reactivity/src/App.vue
+++ vue-reactivity/src/App.vue
@@ -1,61 +1,74 @@
 <template>
   <div>
-    <h1>Cailyn's Kitchen</h1>
-    <Cart
-    :items="items"
-    :total="total"/>
+    <div class="topbar">
+      <h1>Cailyn's Kitchen</h1>
+      <Cart :items="items" :total="total" />
+    </div>
     <Fruit
-    v-for="menu in menus"
-    :name="menu.name"
-    :price="menu.price"
-    @button-clicked="add"/>
-</div>
+      v-for="menu in menus"
+      :key="menu"
+      :img="menu.img"
+      :name="menu.name"
+      :price="menu.price"
+      @button-clicked="add"
+    />
+  </div>
 </template>
 
 <script>
-import Fruit from "./MenuItem.vue";
-import Cart from "./cart.vue";
+import Fruit from './MenuItem.vue'
+import Cart from './cart.vue'
 
 export default {
   components: {
     Fruit,
-    Cart,
+    Cart
   },
-  data () {
+  data() {
     return {
       items: 0,
       total: 0,
       menus: [
-  {
-    img: "https://www.gardeningknowhow.com/wp-content/uploads/2021/07/strawberry.jpg",
-    name: "Strawberry",
-    price: 2,
+        {
+          img: 'https://www.gardeningknowhow.com/wp-content/uploads/2021/07/strawberry.jpg',
+          name: 'Strawberry',
+          price: 2
+        },
+        {
+          img: 'https://billsberryfarm.com/wp-content/uploads/2020/08/blueberry-2.png',
+          name: 'Blueberry',
+          price: 420
+        },
+        {
+          img: 'https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_728062678_8992842f-1a95-4365-b326-93a367a0ab28_1024x1024.jpg?v=1569666151',
+          name: 'Raspberry',
+          price: 88
+        },
+        {
+          img: 'https://cdn.shopify.com/s/files/1/2785/6868/products/43407-crystallized-citrus_blackberry_1c0e8c6c-340f-4228-9655-b659d482524c_1024x1024.jpg?v=1610743085',
+          name: 'Blackberry',
+          price: 999
+        }
+      ]
+    }
   },
-    {
-    img: "https://billsberryfarm.com/wp-content/uploads/2020/08/blueberry-2.png",
-    name: "Blueberry",
-    price: 420,
-  },
- {
-    img: "https://cdn.shopify.com/s/files/1/2336/3219/products/shutterstock_728062678_8992842f-1a95-4365-b326-93a367a0ab28_1024x1024.jpg?v=1569666151",
-    name: "Raspberry",
-    price: 88,
-  },
- {
-    img: "https://cdn.shopify.com/s/files/1/2785/6868/products/43407-crystallized-citrus_blackberry_1c0e8c6c-340f-4228-9655-b659d482524c_1024x1024.jpg?v=1610743085",
-    name: "Blackberry",
-    price: 999,
-}]}
-    },
-    methods: {
-      add() {
-        this.items = this.items + 1
-        this.total = this.total
-      }
-    },
-  };
+  methods: {
+    add() {
+      this.items = this.items + 1
+      this.total = this.total
+    }
+  }
+}
 </script>
 
 <style scoped>
-
+.topbar {
+  height: 100px;
+  width: 100%;
+  position: absolute;
+  display: flex;
+  top: 0px;
+  left: 0px;
+  background-color: rgb(81, 162, 255);
+}
 </style>
--- vue-reactivity/src/MenuItem.vue
+++ vue-reactivity/src/MenuItem.vue
@@ -1,23 +1,62 @@
 <template>
-<div>
-    <img src="" alt="">
-    <h1>{{name}}</h1>
-    <p>${{price}}</p>
-    <button @click="$emit('buttonClicked')">Add to Cart</button>
-</div>
+  <div class="cards">
+    <div class="box">
+      <img :src="img" alt="" />
+      <h1 class="name">{{ name }}</h1>
+      <p class="price">${{ price }}</p>
+      <button @click="$emit('buttonClicked')">Add to Cart</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    Name: "Fruit",
-    props: {
-        img: String,
-        name: String,
-        price: String,
-    },
+  Name: 'Fruit',
+  props: {
+    img: String,
+    name: String,
+    price: String
+  }
 }
 </script>
 
 <style scoped>
+.cards {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
 
+.box {
+  height: 10%;
+  width: 10%;
+  margin: 30px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 20px;
+  border-width: 3px;
+  border-style: solid;
+  border-color: rgb(0, 0, 0);
+}
+
+img {
+  margin: 0 auto;
+  display: block;
+  height: 100px;
+}
+
+.name {
+  font-size: 30px;
+  text-align: center;
+}
+
+.price {
+  font-size: 18px;
+  text-align: center;
+}
+
+.button {
+  display: block;
+  justify-content: center;
+}
 </style>
